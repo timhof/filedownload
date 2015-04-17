d7bed02fc1e9fe53972885382c70fabd63997e57
Timothy Imhof <timothy.imhof@teachforamerica.org>
2015-04-17 17:41:31 -0400

    Fixed JSON conversion

--- src/main/java/org/tfa/rest/SilanisREST.java
+++ src/main/java/org/tfa/rest/SilanisREST.java
@@ -1,7 +1,10 @@
 package org.tfa.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.tfa.dto.SilanisCallbackDTO;
@@ -10,6 +13,7 @@ import org.tfa.dto.SilanisCallbackDTO;
 public class SilanisREST {
 
 		@POST
+	    @Consumes(MediaType.APPLICATION_JSON)
 	    @Path("/callback")
 	    public Response callback(SilanisCallbackDTO dto) {
 			
